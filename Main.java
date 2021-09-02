1f551d8d79f59eebf84f19a301eb885535317cc8
olexiypr <89968042+olexiypr@users.noreply.github.com>
2021-09-02 12:40:18 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.security.MessageDigest;
+
+class Quiz {
+
+    public static void main(String[] args) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] digest = md.digest("abracadabra".getBytes("UTF-8"));
+        for (byte b : digest) {
+            System.out.printf("%02x", b);
+        }
+    }
+}
